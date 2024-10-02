4bad1271005864c27842d8111604019e18395102
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-10-02 18:56:24 -0400

    add db diagram sql

--- C61/Sprint 2/dev/database/green_flag_v2.sql
+++ C61/Sprint 2/dev/database/green_flag_v2.sql
@@ -10,16 +10,44 @@ CREATE TYPE "status" AS ENUM (
   'no'
 );
 
+CREATE TYPE "relationship" AS ENUM (
+  'fun',
+  'shortterm',
+  'longterm'
+);
+
+CREATE TYPE "reason_flagged" AS ENUM (
+  'unavailable',
+  'creep',
+  'unanswering messages'
+);
+
+CREATE TABLE "notification" (
+  "user_id" integer PRIMARY KEY,
+  "read" boolean,
+  "text" text
+);
+
 CREATE TABLE "user" (
-  "id" serial PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "first_name" varchar,
   "last_name" varchar,
+  "password" varchar,
   "email" varchar,
   "date_of_birth" date,
   "gender" gender,
   "preferred_gender" gender,
   "preferred_min_age" integer,
-  "preferred_max_age" integer
+  "preferred_max_age" integer,
+  "relationship_type" relationship,
+  "height" integer,
+  "religion" varchar,
+  "wants_kids" bool,
+  "city" varchar,
+  "last_lat" double,
+  "last_long" double,
+  "token" varchar,
+  "email_confirmed" bool
 );
 
 CREATE TABLE "user_activities" (
@@ -28,55 +56,59 @@ CREATE TABLE "user_activities" (
   PRIMARY KEY ("user_id", "activity_id")
 );
 
+CREATE TABLE "flagged" (
+  "id" integer PRIMARY KEY,
+  "user_id" integer,
+  "reporter_id" integer,
+  "reason" reason_flagged
+);
+
 CREATE TABLE "activity" (
-  "id" serial PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "name" varchar
 );
 
 CREATE TABLE "user_photo" (
-  "id" serial PRIMARY KEY,
   "user_id" integer,
-  "photo_id" integer
+  "photo_id" integer,
+  PRIMARY KEY ("user_id", "photo_id")
 );
 
 CREATE TABLE "photo" (
-  "id" serial PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "encryption_key" varchar,
   "position" integer
 );
 
 CREATE TABLE "match" (
-  "id" serial PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "suggestion_id" integer,
-  "most_recent_suggestion" date,
-  "chatroom_name" varchar,
-  "user_id_1" integer UNIQUE,
-  "user_id_2" integer UNIQUE
+  "chatroom_name" varchar
 );
 
 CREATE TABLE "message" (
-  "id" serial PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "match_id" integer,
-  "receiver_id" integer,
+  "sender_id" integer,
   "text" text NOT NULL,
   "date" date
 );
 
 CREATE TABLE "suggestion" (
-  "id" serial PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "date" date,
   "user_id_1" integer UNIQUE,
   "user_id_2" integer UNIQUE,
   "status" status DEFAULT 'pending'
 );
 
-COMMENT ON COLUMN "user_activities"."user_id" IS 'PK & FK - contrainte d.unicité';
+COMMENT ON COLUMN "user"."token" IS 'regeneré a chaque heure';
 
 COMMENT ON COLUMN "user_activities"."activity_id" IS 'PK & FK - contrainte d.unicité';
 
 COMMENT ON COLUMN "photo"."position" IS 'enum de 1 a 5 ou interval?';
 
-COMMENT ON COLUMN "match"."user_id_1" IS 'contrainte unique ensemble';
+COMMENT ON COLUMN "match"."chatroom_name" IS 'maybe chatroom id and message points to a chatroom id';
 
 COMMENT ON TABLE "message" IS 'fonction - exiger acces au BE via table message';
 
@@ -88,11 +120,7 @@ ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "user_activities" ("user_id
 
 ALTER TABLE "activity" ADD FOREIGN KEY ("id") REFERENCES "user_activities" ("activity_id");
 
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "match" ("user_id_1");
-
 ALTER TABLE "match" ADD FOREIGN KEY ("id") REFERENCES "message" ("match_id");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "match" ("user_id_2");
 
 ALTER TABLE "suggestion" ADD FOREIGN KEY ("id") REFERENCES "match" ("suggestion_id");
 
@@ -100,8 +128,14 @@ ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "suggestion" ("user_id_1");
 
 ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "suggestion" ("user_id_2");
 
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "message" ("receiver_id");
+ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "message" ("sender_id");
 
 ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "user_photo" ("user_id");
 
 ALTER TABLE "photo" ADD FOREIGN KEY ("id") REFERENCES "user_photo" ("photo_id");
+
+ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "flagged" ("user_id");
+
+ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "flagged" ("reporter_id");
+
+ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "notification" ("user_id");
