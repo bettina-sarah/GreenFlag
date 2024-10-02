a7304c20113567cbba64a919aa9aa687bd30fdf0
Vincent <genesisqc@hotmail.ca>
2024-10-02 17:20:42 -0400

    ADD frontend backend frames and testconnection

---
+++ C61/Sprint 2/dev/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TestConnection from './TestConnection';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Home</h1>
+      <TestConnection/>
+    </div>
+  );
+}
+
+export default App;
