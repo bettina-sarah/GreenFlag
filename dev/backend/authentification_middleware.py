47bada83af22254aa00c9211ca7ad1332bd26819
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-10-04 15:38:30 -0400

    account manager

--- dev/backend/authentification_middleware.py
+++ dev/backend/authentification_middleware.py
@@ -1,14 +1,20 @@
 from utility_functions import requires_token
-from crypt_keeper import CryptKeeper
 # decorator
 
-class AuthentificationMiddleware:
+class AccountManager:
     def __init__(self) -> None:
         pass
     
-    def check_session_validity(self) -> bool:
-        crypt = CryptKeeper
-        reponse = crypt.encode('hello')
-        
-        # ou... CryptKeeper.encore()
+    def login(self) -> bool:
+        pass
     
+    @requires_token
+    def modify_profile(self) -> bool:
+        pass
+    
+    @requires_token
+    def modify_profile(self) -> bool:
+        pass
+    
+    def delete_account(self) -> bool:
+        pass
