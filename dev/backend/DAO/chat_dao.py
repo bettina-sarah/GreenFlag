234c2e0b56b40d0efa40f498506555c905e930ab
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-10-05 22:42:10 -0400

    dao frames finished - account,chat,matching,notification,stats

---
+++ dev/backend/DAO/chat_dao.py
@@ -0,0 +1,31 @@
+from dao import DAO
+from types import List
+
+class ChatDAO(DAO):
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod   
+    def create_connection(params: dict ) -> None:
+        pass    
+
+    # all dao methods need to be redefined here?
+
+    def create_chat(self) -> bool:
+        pass
+
+    def update_chat(self) -> bool:
+        pass
+
+    def delete_chat(self) -> bool:
+        pass
+
+    def get_chatroom_id(self, user_id):
+        pass
+
+    def send_message(self, chat_id, message) -> bool:
+        pass
+
+    def get_messages(self, chat_id) -> List[str]:
+        pass
