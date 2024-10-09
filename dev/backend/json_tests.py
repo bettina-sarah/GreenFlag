3fb258795c1b6dbfd0130fd067540478276e21cc
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-10-09 13:24:45 -0400

    account mngr refactor

---
+++ dev/backend/json_tests.py
@@ -0,0 +1,16 @@
+json_create = {
+                    'firstname': 'haha',
+        'lastname': 'hehe',
+        'email': 'haaaa@email.com',
+        'password': '1234'}
+    
+json_delete = {
+        'email': 'haaaa@email.com',
+        'password': '1234'}
+
+
+json_modify = {
+                    'firstname': 'new name',
+        'lastname': 'NEW',
+        'email': 'user@email.com',
+        'password': '1234'}
