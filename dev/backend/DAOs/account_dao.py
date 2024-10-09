016f22e35c2b246817f84dddf989aeb1e2da9e73
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-10-09 11:14:00 -0400

    refactor dao, account_dao & create,delete,login, get profile FONCTIONNEL back & front

--- dev/backend/DAOs/account_dao.py
+++ dev/backend/DAOs/account_dao.py
@@ -11,28 +11,30 @@ class AccountDAO(DAO):
     def login(params:tuple) -> bool:
         query = 'SELECT * FROM member WHERE email = %s and member_password = %s'
         response = AccountDAO.prepare_statement(query, params)
+        # user returned ici : 
         return response
 
 
     @staticmethod
     def create_account(params:tuple) -> bool:
             query = 'INSERT INTO member (first_name, last_name, email, member_password) VALUES (%s, %s, %s, %s) RETURNING id;'
-            response = AccountDAO.prepare_statement(query, params)
+            response = AccountDAO.prepare_statement("insert", query, params)
+            # si courriel existe deja, DB retorune erreur cle existe - faut gerer - envoyer au frontend 'account exists' 
             return response
     
     # rudimentaire: version finale faut que ca delete la personne des tables de suggestions de tout le monde, les match, les messages, les photos dans berkeleyDB
     @staticmethod
     def delete_account(params:tuple) -> bool:
         query = 'DELETE FROM member where email = %s and member_password = %s;'
-        response = AccountDAO.prepare_statement(query, params)
+        response = AccountDAO.prepare_statement("delete", query, params)
         return response
     
     
     # this can be in DAO directly 
-    def prepare_statement(query, params) -> bool:
+    def prepare_statement(request_type, query, params) -> bool:
         try:
-            connection = DAO.get_connection()
-            response = DAO.send_request(connection, query, params)
+            connection = AccountDAO.get_connection()
+            response = AccountDAO.send_request(request_type, connection, query, params)
             connection.commit() # possibly necessary for an insert request
             return response
         except Exception as error:  
@@ -41,7 +43,7 @@ class AccountDAO(DAO):
     
     def get_profile(params:tuple) -> List[tuple]:
         query = 'SELECT * FROM member where email = %s;'
-        response = AccountDAO.prepare_statement(query, params)
+        response = AccountDAO.prepare_statement("select", query, params)
         return response
     
         # try:
