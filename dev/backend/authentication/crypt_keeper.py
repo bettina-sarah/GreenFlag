7106b748fe0b7278524c478e48997f63954b076c
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-10-17 14:10:37 -0400

    validate jwt, format, header, decode payload

--- dev/backend/authentication/crypt_keeper.py
+++ dev/backend/authentication/crypt_keeper.py
@@ -6,6 +6,7 @@ import json
 import jwt
 from datetime import datetime, timezone
 import time
+import base64
 
 ''' JWT:   
 [header].[payload].[signature]
@@ -66,7 +67,6 @@ class CryptKeeper:
     
 
     def encode(self,json_information) -> str:
-        # add expiry date
         final_json = self.expiry_date(json_information)  
         encoded_jwt = jwt.encode(final_json, "secret", algorithm="HS256")
         return encoded_jwt
@@ -77,6 +77,51 @@ class CryptKeeper:
         expiry_timestamp = current_timestamp + 7200
         json['exp'] = expiry_timestamp
         return json
+
+    def validate_jwt(self,token) -> bool:
+        try:
+            header, payload, signature = self.validate_format(token)
+            decoded_header = self.decode_header(header)
+            if self.validate_header(decoded_header):
+                decoded_payload = self.decode(payload)
+                if decoded_payload is not None:  # Check if the payload was valid
+                    return True
+                return True
+        except Exception as e:
+            print(e)
+        return False
+        
+    def validate_format(self,token) -> bool:
+        parts = token.split('.')
+        if len(parts) == 3:
+            return parts
+        return False
+
+    def decode_header(self, header: str) -> dict:
+        # Decode Base64URL encoded header
+        padding = '=' * (4 - len(header) % 4) if len(header) % 4 else ''
+        decoded = base64.urlsafe_b64decode(header + padding).decode('utf-8')
+        return json.loads(decoded)
+
+    def validate_header(self,header) -> bool:
+        header_data = json.loads(header)
+        known_algorithms = ['HS256', 'RS256']  # Example known algorithms
+        known_types = ['JWT']
+    
+        if header_data.get('typ') not in known_types or header_data.get('alg') not in known_algorithms:
+            return False
+        return True
+    
+    # def validate_payload(self, payload) -> bool:
+    #     # Decode Base64URL encoded payload
+    #     padding = '=' * (4 - len(payload) % 4) if len(payload) % 4 else ''
+    #     decoded_payload = base64.urlsafe_b64decode(payload + padding).decode('utf-8')
+    #     payload_data = json.loads(decoded_payload)
+        
+    #     return True
+
+    
+        
     
     
 if __name__ == "__main__":
@@ -84,6 +129,9 @@ if __name__ == "__main__":
     jsonn = {"some": "payload"}
     result = crypt.encode(jsonn)
     print(result)
-    decoded = crypt.decode(result)
-    print(decoded)
+    print(crypt.validate_jwt(result))
+    # decoded = crypt.decode(result)
+    # print(decoded)
+    
+    
 
