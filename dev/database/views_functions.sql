226eb70fd39146f4a267435aa338c7fa09bbb60e
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-10-21 12:39:59 -0400

    add photo sql function

--- dev/database/views_functions.sql
+++ dev/database/views_functions.sql
@@ -22,3 +22,25 @@ FROM
   member AS m
 INNER JOIN member_activities AS ma ON m.id = ma.member_id
 INNER JOIN activity AS a ON ma.activity_id = a.id;
+
+DROP FUNCTION IF EXISTS add_photos;
+
+CREATE OR REPLACE FUNCTION add_photos
+(user_id INTEGER, photo_key VARCHAR(128), photo_number INTEGER) --photo number donné par backend 
+RETURN BOOLEAN
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+  --delete all photos
+  DELETE from member_photo USING member 
+  WHERE member_photo.member_id = member.id AND member.user_id = user_id;
+  -- insert into photo
+  FOR i IN 1..photo_number BY 1 LOOP
+    @photo_id = INSERT INTO photo (encryption_key, position) VALUES (photo_key, 1) RETURNING photo_id;
+    --insert into member_photo
+    INSERT INTO member_photo (member_id, photo_id) VALUES (user_id, @photo_id)
+    END LOOP;
+    -- return TRUE / FALSE
+    RETURN TRUE;
+  RETURN FALSE;
+END$$;
