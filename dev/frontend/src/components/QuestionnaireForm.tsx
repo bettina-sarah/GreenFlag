62b2bf217b4841962b7aade66f79e09674462020
Vincent Fournier <genesisqc@hotmail.ca>
2024-10-22 22:36:12 -0400

    REFACTOR QuestionnaireForm division logic/render

--- dev/frontend/src/components/QuestionnaireForm.tsx
+++ dev/frontend/src/components/QuestionnaireForm.tsx
@@ -1,10 +1,9 @@
-import { IP_SERVER } from "@/config/constants";
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-import axios from 'axios';
+import { Controller, useForm } from "react-hook-form";
 import {Datepicker, Checkbox, Label, Select, TextInput, Dropdown, Textarea } from "flowbite-react";
 import ImageInputCustom from "./ImageInput";
+import { FormDataHobbies, FormDataInfo, FormDataPhoto, onSubmitFormHobbies, onSubmitFormInfo, onSubmitPhoto } from "./form_submits/QuestionnaireSubmitHandlers";
 // https://marmelab.com/react-admin/ImageInput.html
 
 const QuestionnaireForm = () => {
@@ -15,62 +14,24 @@ const QuestionnaireForm = () => {
         "hiking", "yoga", "photography", "cooking", "traveling", "reading", "videogaming", "biking", "running",
         "watchingmovies", "workingout", "dancing", "playinginstrument", "attendingconcerts", "painting", "volunteering",
         "playingsports", "crafting", "petlover", "learningnewlanguage"
-        ];
-
-
+        ] as const;
     const religions = [
         "Atheist", "Spiritual", "Christian", "Muslim", "Jewish", "Hindu", "Buddhist",
-        "Sikh", "Taoist", "Shinto", "Confucian", "Bahai", "Pagan", "Agnostic", "Other"
-    ];
-    
+        "Sikh", "Taoist", "Shinto", "Confucian","Bahai","Pagan","Agnostic","Other"]
+
     const [selectedGenders, setSelectedGenders] = useState<string[]>([]);
+
     const toggleGender = (gender: string) => {
         setSelectedGenders((prev) => 
             prev.includes(gender) ? prev.filter((g) => g !== gender) : [...prev, gender]
         );
     };
 
-
-    type FormDataInfo = {
-        // Infos
-        date_of_birth: Date | null
-        gender: string
-        height: number
-        religion: string
-        want_kids: boolean
-        city: string
-        bio: string
-        // Preferences
-        min_age: number
-        max_age: number
-        relationship_type: string
-        preferred_genders: string[]
-    }
-
-    // type FormDataPhotos = {
-    //     Photos: File[] | null
-    // }
-
-    interface FormDataPhoto {
-        image: File | null
-    }
-
     const {
         register: registerHobbies,
         handleSubmit :handleSubmitHobbies,
         formState: { errors: errorsHobbies },
     } = useForm<FormDataHobbies>()
-    const onSubmitFormHobbies = handleSubmitHobbies( async (data)=> {
-        try{
-            const answer = await axios.post(IP_SERVER+'/hobbies',data)
-            if (answer.data){
-                console.log(answer);
-                navigate('/matching')
-            }
-        } catch(error){
-            console.error('Error during account modification:',error)
-        }
-    } )
 
     const {
         register: registerInfo,
@@ -78,59 +39,13 @@ const QuestionnaireForm = () => {
         handleSubmit: handleSubmitInfo,
         formState: { errors: errorsInfo },
     } = useForm<FormDataInfo>()
-    const onSubmitFormInfo = handleSubmitInfo( async (data)=> {
-        try{
-            const answer = await axios.post(IP_SERVER+'/questionnaire',data)
-            if (answer.data){
-                console.log(answer);
-                navigate('/matching')
-            }
-        } catch(error){
-            console.error('Error during account modification:',error)
-        }
-    } )
-
-    // const {
-    //     control: controlPhotos,
-    //     handleSubmit: handleSubmitPhotos,
-    //     formState: { errors: errorsPhotos },
-    // } = useForm<FormDataPhotos>()
-    // const onSubmitFormPhotos = handleSubmitPhotos( async (data)=> {
-    //     try{
-    //         const answer = await axios.post(IP_SERVER+'/questionnaire',data)
-    //         if (answer.data){
-    //             console.log(answer);
-    //             navigate('/matching')
-    //         }
-    //     } catch(error){
-    //         console.error('Error during account modification:',error)
-    //     }
-    // } )
 
     const {control, handleSubmit: handleSubmitPhoto} = useForm<FormDataPhoto>();
-
-    const onSubmitPhoto: SubmitHandler<FormDataPhoto> = async (data) => {
-        
-        try{
-            const formData = new FormData();
-            if(data.image){
-                formData.append('image',data.image);
-                const answer = await axios.post(IP_SERVER+'/photo',formData)
-                if (answer.data){
-                    console.log(answer);
-                    console.log(data)
-                }
-            }    
-        } catch(error){
-            console.error('Error during account modification:',error)
-        }
-    }
-
 
     return (
         <div className="flex flex-col justify-between m-2 overflow-visible">
 
-            <form onSubmit={onSubmitFormHobbies} >
+            <form onSubmit={handleSubmitHobbies((data)=> onSubmitFormHobbies(data,navigate))} >
                 <div className="flex flex-col space-y-4 p-3 m-4">
                     {hobbiesKeys.map((hobby) =>(
                         <div className="flex items-center gap-2">
@@ -138,93 +53,13 @@ const QuestionnaireForm = () => {
                             <Label htmlFor={hobby}>{hobby}</Label>
                         </div>
                     ))}
-                    {/* <div className="flex items-center gap-2">
-                        <Checkbox id="hiking" {...registerHobbies("hiking")}/>
-                        <Label htmlFor="hiking">Hiking</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="yoga" {...registerHobbies("yoga")} />
-                        <Label htmlFor="yoga">Yoga or Meditation</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="photography" {...registerHobbies("photography")} />
-                        <Label htmlFor="photography">Photography</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="cooking" {...registerHobbies("cooking")} />
-                        <Label htmlFor="cooking">Cooking or Baking</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="traveling" {...registerHobbies("traveling")} />
-                        <Label htmlFor="traveling">Traveling</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="reading" {...registerHobbies("reading")} />
-                        <Label htmlFor="reading">Reading Books</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="videogaming" {...registerHobbies("videogaming")} />
-                        <Label htmlFor="videogaming">Video Gaming</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="biking" {...registerHobbies("biking")} />
-                        <Label htmlFor="biking">Biking or Cycling</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="running" {...registerHobbies("running")} />
-                        <Label htmlFor="running">Running or Jogging</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="watchingmovies" {...registerHobbies("watchingmovies")} />
-                        <Label htmlFor="watchingmovies">Watching Movies or TV Shows</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="workingout" {...registerHobbies("workingout")} />
-                        <Label htmlFor="workingout">Working Out / Fitness</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="dancing" {...registerHobbies("dancing")} />
-                        <Label htmlFor="dancing">Dancing</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="playinginstrument" {...registerHobbies("playinginstrument")} />
-                        <Label htmlFor="playinginstrument">Playing Musical Instrument</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="attendingconcerts" {...registerHobbies("attendingconcerts")} />
-                        <Label htmlFor="attendingconcerts">Attending Concerts or Festivals</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="painting" {...registerHobbies("painting")} />
-                        <Label htmlFor="painting">Painting or Drawing</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="volunteering" {...registerHobbies("volunteering")} />
-                        <Label htmlFor="volunteering">Volunteering</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="playingsports" {...registerHobbies("playingsports")} />
-                        <Label htmlFor="playingsports">Playing Sports</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="crafting" {...registerHobbies("crafting")} />
-                        <Label htmlFor="crafting">Crafting or DIY Projects</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="petlover" {...registerHobbies("petlover")} />
-                        <Label htmlFor="petlover">Pet Lover or Animal Care</Label>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="learningnewlanguage" {...registerHobbies("learningnewlanguage")} />
-                        <Label htmlFor="learningnewlanguage">Learning New Languages</Label>
-                    </div> */}
                 </div>
 
                 <button className="bg-teal-600 p-1 rounded-md text-white" type="submit">
                     Submit
                 </button>
             </form>
-            <form  onSubmit={onSubmitFormInfo}>
+            <form  onSubmit={handleSubmitInfo((data)=> onSubmitFormInfo(data,navigate))}>
                 <label>Select Your Date of Birth:</label>
                 <Controller
                     name="date_of_birth"
@@ -246,10 +81,6 @@ const QuestionnaireForm = () => {
                     {genders.map((gender)=>(
                         <option value={gender}>{gender}</option>
                     ))}
-                    {/* <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="Non-binary">Non-Binary</option>
-                    <option value="Other">Other</option> */}
                 </Select>
                 {errorsInfo.gender && errorsInfo.gender.type === "required" && (
                     <span>This is required</span>
@@ -267,21 +98,6 @@ const QuestionnaireForm = () => {
                     {religions.map((religion)=>(
                         <option value={religion}>{religion}</option>
                     ))}
-                    {/* <option value="Atheist">Atheist</option>
-                    <option value="Spiritual">Spiritual</option>
-                    <option value="Christian">Christian</option>
-                    <option value="Muslim">Muslim</option>
-                    <option value="Jewish">Jewish</option>
-                    <option value="Hindu">Hindu</option>
-                    <option value="Buddhist">Buddhist</option>
-                    <option value="Sikh">Sikh</option>
-                    <option value="Taoist">Taoist</option>
-                    <option value="Shinto">Shinto</option>
-                    <option value="Confucian">Confucian</option>
-                    <option value="Bahai">Bahai</option>
-                    <option value="Pagan">Pagan</option>
-                    <option value="Agnostic">Agnostic</option>
-                    <option value="Other">Other</option> */}
                 </Select>
                 {errorsInfo.religion && errorsInfo.religion.type === "required" && (
                     <span>This is required</span>
@@ -384,43 +200,6 @@ const QuestionnaireForm = () => {
                     Submit
                 </button>
             </form>
-            {/* <form onSubmit={onSubmitFormPhotos}>
-                <Controller
-                    name="Photos"
-                    control={controlPhotos}
-                    defaultValue={[]}
-                    render={({ field }) =>(
-                        <ImageInput
-                            {...field}
-                            multiple
-                            accept={{
-                                'image/png': ['.png'],
-                                'image/jpeg': ['.jpg', '.jpeg'],
-                              }}
-                            label="Upload an image"
-                            maxSize={1024 * 1024}
-                            onChange={(event) =>{
-                                if (event.target.files){
-                                    const files = Array.from(event.target.files)
-                                    onChange(files);
-                                }
-                            }}
-                        >
-                            {value && value.map((file,index) =>(
-                                <ImageField
-                                 key={index}
-                                 source={URL.createObjectURL(file)}
-                                 title={file.name}
-                                />
-                            ))}
-                        </ImageInput>
-                    )}
-                />
-
-                <button className="bg-teal-600 p-1 rounded-md text-white" type="submit">
-                    Submit
-                </button>
-            </form> */}
 
             <form onSubmit={handleSubmitPhoto(onSubmitPhoto)}>
                 <ImageInputCustom name="image" control={control}/>
---
+++ dev/frontend/src/components/form_submits/QuestionnaireSubmitHandlers.ts
@@ -0,0 +1,96 @@
+import { IP_SERVER } from "@/config/constants";
+import axios from 'axios';
+import { NavigateFunction } from 'react-router-dom';
+
+
+export type FormDataHobbies = {
+    // Activities
+    hiking: boolean
+    yoga: boolean
+    photography: boolean
+    cooking: boolean
+    traveling: boolean
+    reading: boolean
+    videogaming: boolean
+    biking: boolean
+    running: boolean
+    watchingmovies: boolean
+    workingout: boolean
+    dancing: boolean
+    playinginstrument: boolean
+    attendingconcerts: boolean
+    painting: boolean
+    volunteering: boolean
+    playingsports: boolean //(e.g., Soccer, Tennis, Basketball)
+    crafting: boolean
+    petlover: boolean
+    learningnewlanguage: boolean
+}
+
+export type FormDataInfo = {
+    // Infos
+    date_of_birth: Date | null
+    gender: string
+    height: number
+    religion: string
+    want_kids: boolean
+    city: string
+    bio: string
+    // Preferences
+    min_age: number
+    max_age: number
+    relationship_type: string
+    preferred_genders: string[]
+}
+
+export type FormDataPhoto = {
+    image: File | null
+}
+
+export const onSubmitFormHobbies =  async (
+    data:FormDataHobbies,
+    navigate:NavigateFunction
+)=> {
+    try{
+        const answer = await axios.post(IP_SERVER+'/hobbies',data)
+        if (answer.data){
+            console.log(answer);
+            navigate('/matching')
+        }
+    } catch(error){
+        console.error('Error during account modification:',error)
+    }
+} 
+
+export const onSubmitFormInfo = async (
+    data:FormDataInfo,
+    navigate:NavigateFunction
+)=> {
+    try{
+        const answer = await axios.post(IP_SERVER+'/questionnaire',data)
+        if (answer.data){
+            console.log(answer);
+            navigate('/matching')
+        }
+    } catch(error){
+        console.error('Error during account modification:',error)
+    }
+}
+
+export const onSubmitPhoto = async (
+    data:FormDataPhoto
+) => {
+    try{
+        const formData = new FormData();
+        if(data.image){
+            formData.append('image',data.image);
+            const answer = await axios.post(IP_SERVER+'/photo',formData)
+            if (answer.data){
+                console.log(answer);
+                console.log(data)
+            }
+        }    
+    } catch(error){
+        console.error('Error during account modification:',error)
+    }
+}
