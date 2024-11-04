ad28b113bb9326a487b04e9472af77e39517afd8
Vincent Fournier <genesisqc@hotmail.ca>
2024-11-03 21:14:47 -0500

    Chatrooms components

--- dev/frontend/src/pages/ChatroomsPage.tsx
+++ dev/frontend/src/pages/ChatroomsPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Menu from '@/components/Menu';
 
 const ChatroomsPage: React.FC = () => (
 	<div>
 		<Menu />
-		<h1>Chatrooms</h1>
+		
 
 	</div>
 );
