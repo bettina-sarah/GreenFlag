285b3c275a7a426a0e8a237ff59d98b45ee73676
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-11-06 19:38:23 -0500

    damned carousel working again

--- dev/frontend/src/components/profile_card_components/PhotoCarousel.tsx
+++ dev/frontend/src/components/profile_card_components/PhotoCarousel.tsx
@@ -1,43 +1,81 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import Image from '../../../ressources/avatars/AdobeStock_222851624_Preview.jpeg';
 // import Image2 from '../../../ressources/avatars/charlesdeluvio-kVg2DQTAK7c-unsplash.jpg';
 // import Image3 from '../../../ressources/avatars/oguz-yagiz-kara-uTlMt9o7SHE-unsplash.jpg';
 import unknown_photo from "../../../ressources/avatars/unknown_photo.jpg";
-import Image from "../Image";
-import { Carousel } from "@mantine/carousel";
+// import Image from "../Image";
+import { Carousel } from "flowbite-react";
+// import useFetch from "@/api/useFetch";
+import fetchData from "@/api/fetchData";
 
 interface CarouselProps {
   images: string[] | null;
 }
 
+interface IPhotoData {
+  path: string;
+  key: string;
+}
+
 const PhotoCarousel: React.FC<CarouselProps> = ({ images }) => {
-  return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel>
-        {!images ||
-          (images.length === 0 && (
-            <Carousel.Slide>
-              <img
-                className="w-full object-cover"
-                src={unknown_photo}
-                alt="First slide"
-              />
-            </Carousel.Slide>
+  const [photoData, setPhotoData] = React.useState<IPhotoData[]>([]);
+
+  useEffect(() => {
+    if (images && images.length > 0) {
+      const fetchPhotos = async () => {
+        try {
+          const fetchedPhotos: IPhotoData[] = await Promise.all(
+            images.map(async (key: string) => {
+              const photo = await fetchData<IPhotoData>("/get-photo", key);
+              return { key, path: photo.path };
+            })
+          );
+          setPhotoData(fetchedPhotos);
+        } catch (error) {
+          console.error("Error fetching photos:", error);
+        }
+      };
+      fetchPhotos();
+    } else {
+      setPhotoData([]);
+    }
+  }, []); //[profileData, profileLoading, profileError]
+
+  if (!images || images.length === 0) {
+    return (
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slide={false}>
+          <img
+            className="w-full object-cover"
+            src={unknown_photo}
+            alt="First slide"
+          />
+          <img
+            className="w-full object-cover"
+            src={unknown_photo}
+            alt="unknown"
+          />
+        </Carousel>
+      </div>
+    );
+  } else if (photoData && photoData.length > 0) {
+    return (
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slide={false}>
+          {photoData.map((image) => (
+            <img
+              key={image.key}
+              className="w-full object-cover"
+              src={image.path}
+              alt="unknown"
+            />
           ))}
-        {images &&
-          images.map((imageKey, index) => (
-            <Carousel.Slide key={imageKey}>
-              <Image
-                key={imageKey}
-                className="w-full object-cover"
-                imageKey={imageKey}
-                altText={`Slide ${index}`}
-              />
-            </Carousel.Slide>
-          ))}
-      </Carousel>
-    </div>
-  );
+        </Carousel>
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 };
 
 export default PhotoCarousel;
@@ -67,3 +105,4 @@ export default PhotoCarousel;
 // };
 
 // export default PhotoCarousel;
+//
--- dev/frontend/src/pages/MatchingPage.tsx
+++ dev/frontend/src/pages/MatchingPage.tsx
@@ -38,7 +38,7 @@ const MatchingPage: React.FC = () => {
     error: profileError,
   } = useFetch<IProfileData[]>({
     url: "/suggestions",
-    data: { id: "1" },
+    data: { id: "8" },
   });
 
   if (!profileData && profileLoading) {
