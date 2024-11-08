bbc51af36c830c734f54d9d89dc36cbe12c38131
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-11-07 21:17:40 -0500

    tinder card WORKS

--- dev/frontend/src/components/ProfileCard.tsx
+++ dev/frontend/src/components/ProfileCard.tsx
@@ -9,6 +9,7 @@ import IconButton from "./IconButton";
 import RedFlag from "../../ressources/icons/FlagButton_left.png";
 import GreenFlag from "../../ressources/icons/FlagButton_right.png";
 import Undo from "../../ressources/icons/undo.png";
+import TinderCard from "react-tinder-card";
 
 interface IProfileProps {
   profile_info: ProfileProps;
@@ -30,20 +31,24 @@ const UndoSwipe = () => {
 const ProfileCard: React.FC<IProfileProps> = ({ profile_info, photos }) => {
   console.log(photos);
   return (
-    <div className="w-96 bg-greenflag-green p-1 rounded">
-      <PhotoCarousel images={photos} />
-      <BasicInfo basic_info={profile_info.basic_info} />
-      <RelationshipGoals
-        relationship={profile_info.relationship}
-        wants_kids={profile_info.wants_kids}
-      />
-      <Hobbies hobbies={profile_info.hobby_array} />
-      <Bio bio={profile_info.bio} />
-      <div className="flex items-center justify-evenly p-3">
-        <IconButton icon={RedFlag} onClick={SwipeRight} />
-        <IconButton icon={Undo} onClick={UndoSwipe} />
-        <IconButton icon={GreenFlag} onClick={SwipeLeft} />
-      </div>
+    <div>
+      <TinderCard preventSwipe={["right", "left"]}>
+        <div className="w-96 bg-greenflag-green p-1 rounded">
+          <PhotoCarousel images={photos} />
+          <BasicInfo basic_info={profile_info.basic_info} />
+          <RelationshipGoals
+            relationship={profile_info.relationship}
+            wants_kids={profile_info.wants_kids}
+          />
+          <Hobbies hobbies={profile_info.hobby_array} />
+          <Bio bio={profile_info.bio} />
+          <div className="flex items-center justify-evenly p-3">
+            <IconButton icon={RedFlag} onClick={SwipeRight} />
+            <IconButton icon={Undo} onClick={UndoSwipe} />
+            <IconButton icon={GreenFlag} onClick={SwipeLeft} />
+          </div>
+        </div>
+      </TinderCard>
     </div>
   );
 };
