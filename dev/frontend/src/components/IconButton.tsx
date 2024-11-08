cf501cda4475d17e979b5c5ffef79d9e4c4f120c
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-11-07 20:53:55 -0500

    icon buttons added back - functional

--- dev/frontend/src/components/IconButton.tsx
+++ dev/frontend/src/components/IconButton.tsx
@@ -12,7 +12,7 @@ interface IconButtonProps {
 
 const IconButton: React.FC<IconButtonProps> = ({
   icon,
-  page = null,
+  page,
   onClick,
   toggleState,
 }) => {
@@ -26,6 +26,7 @@ const IconButton: React.FC<IconButtonProps> = ({
 
     // If a custom onClick function is provided, execute it
     if (onClick) {
+      console.log("onclick");
       onClick(); // !! when i define my button, i can provide it any function i want !!!
     }
 
@@ -42,9 +43,9 @@ const IconButton: React.FC<IconButtonProps> = ({
 
   return (
     <button
-      className="w-10 h-12 flex items-center justify-center p-1 border-none
+      className="w-15 h-12 flex items-center justify-center p-1 border-none
     hover:bg-teal-500 rounded"
-      onClick={() => handleClick}
+      onClick={handleClick}
     >
       <Icon icon={icon} />
     </button>
--- dev/frontend/src/components/ProfileCard.tsx
+++ dev/frontend/src/components/ProfileCard.tsx
@@ -5,29 +5,28 @@ import RelationshipGoals from "./profile_card_components/RelationshipGoals";
 import Hobbies from "./profile_card_components/Hobbies";
 import Bio from "./profile_card_components/Bio";
 import { ProfileProps } from "@/pages/MatchingPage";
-
-// interface ProfileProps {
-//   user: {
-//     basic_info: {
-//       first_name: string;
-//       age: number;
-//       city: string;
-//       location: number;
-//     };
-//     relationship: string;
-//     wants_kids: boolean;
-//     hobby_array: string[];
-//     bio: string | null;
-//   };
-//   photos: any;
-// }
+import IconButton from "./IconButton";
+import RedFlag from "../../ressources/icons/FlagButton_left.png";
+import GreenFlag from "../../ressources/icons/FlagButton_right.png";
+import Undo from "../../ressources/icons/undo.png";
 
 interface IProfileProps {
   profile_info: ProfileProps;
   photos: string[];
 }
 
-// ({user})
+const SwipeRight = () => {
+  console.log("swiped right");
+};
+
+const SwipeLeft = () => {
+  console.log("swiped left");
+};
+
+const UndoSwipe = () => {
+  console.log("undo");
+};
+
 const ProfileCard: React.FC<IProfileProps> = ({ profile_info, photos }) => {
   console.log(photos);
   return (
@@ -40,11 +39,11 @@ const ProfileCard: React.FC<IProfileProps> = ({ profile_info, photos }) => {
       />
       <Hobbies hobbies={profile_info.hobby_array} />
       <Bio bio={profile_info.bio} />
-      {/* <div className="flex items-center justify-evenly p-3">
+      <div className="flex items-center justify-evenly p-3">
         <IconButton icon={RedFlag} onClick={SwipeRight} />
         <IconButton icon={Undo} onClick={UndoSwipe} />
         <IconButton icon={GreenFlag} onClick={SwipeLeft} />
-      </div> */}
+      </div>
     </div>
   );
 };
