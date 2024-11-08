a4784a59aac025524fca29232653afa2a0ca5935
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-11-07 20:47:51 -0500

    fixed carousel with separate fetch, multiple photos

--- dev/frontend/src/components/ProfileCard.tsx
+++ dev/frontend/src/components/ProfileCard.tsx
@@ -4,36 +4,47 @@ import BasicInfo from "./profile_card_components/BasicInfo";
 import RelationshipGoals from "./profile_card_components/RelationshipGoals";
 import Hobbies from "./profile_card_components/Hobbies";
 import Bio from "./profile_card_components/Bio";
+import { ProfileProps } from "@/pages/MatchingPage";
 
-interface ProfileProps {
-  user: {
-    basic_info: {
-      first_name: string;
-      age: number;
-      city: string;
-      location: number;
-    };
-    relationship: string;
-    wants_kids: boolean;
-    hobby_array: string[];
-    bio: string | null;
-  };
-  photos: any;
+// interface ProfileProps {
+//   user: {
+//     basic_info: {
+//       first_name: string;
+//       age: number;
+//       city: string;
+//       location: number;
+//     };
+//     relationship: string;
+//     wants_kids: boolean;
+//     hobby_array: string[];
+//     bio: string | null;
+//   };
+//   photos: any;
+// }
+
+interface IProfileProps {
+  profile_info: ProfileProps;
+  photos: string[];
 }
 
 // ({user})
-const ProfileCard: React.FC<ProfileProps> = ({ user, photos }) => {
+const ProfileCard: React.FC<IProfileProps> = ({ profile_info, photos }) => {
   console.log(photos);
   return (
     <div className="w-96 bg-greenflag-green p-1 rounded">
       <PhotoCarousel images={photos} />
-      <BasicInfo basic_info={user.basic_info} />
+      <BasicInfo basic_info={profile_info.basic_info} />
       <RelationshipGoals
-        relationship={user.relationship}
-        wants_kids={user.wants_kids}
+        relationship={profile_info.relationship}
+        wants_kids={profile_info.wants_kids}
       />
-      <Hobbies hobbies={user.hobby_array} />
-      <Bio bio={user.bio} />
+      <Hobbies hobbies={profile_info.hobby_array} />
+      <Bio bio={profile_info.bio} />
+      {/* <div className="flex items-center justify-evenly p-3">
+        <IconButton icon={RedFlag} onClick={SwipeRight} />
+        <IconButton icon={Undo} onClick={UndoSwipe} />
+        <IconButton icon={GreenFlag} onClick={SwipeLeft} />
+      </div> */}
     </div>
   );
 };
