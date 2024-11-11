5b01ad2bac381c5925a8e37c29b6e405e436af9d
Vincent <genesisqc@hotmail.ca>
2024-11-11 12:03:53 -0500

    UPDATE chatmenu

--- dev/frontend/src/components/chat_components/MenuChat.tsx
+++ dev/frontend/src/components/chat_components/MenuChat.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import IconButton from '../IconButton';
 import matchingIcon from '../../ressources/icons/matching.png'
 import messageIcon from '../../ressources/icons/messages.png'
 import settingsIcon from '../../ressources/icons/settings.png'
 import logoutIcon from '../../ressources/icons/logout.png'
+import useFetch from '@/api/useFetch';
 
 interface MenuChatProp{
     subject_id: number;
 }
 
 interface Subject{
-
+    id: string;
 }
+
+interface IPhotoData {
+    path: string;
+    key: string;
+  }
 
 
 const MenuChat: React.FC<MenuChatProp> = ({subject_id}) => {
@@ -23,19 +29,25 @@ const MenuChat: React.FC<MenuChatProp> = ({subject_id}) => {
         } = useFetch<Subject>({
         url: "//get-profile",
         data: { id: subject_id },
-      });
+    });
+
+    useEffect(() => {
+        if (subjectData && !subjectLoading && !subjectError) {
+            const photoKeys = subjectData?.user_infos?.photo_keys;
 
 
+        }
+    }, [subjectData, subjectLoading, subjectError, subject_id]);
 
     return(
-    <div className='w-full h-50'>
-       	<div className='flex w-full h-full justify-evenly  bg-greenflag-green'>
-        <IconButton icon={matchingIcon} page="matching"/>
-        <IconButton icon={messageIcon} page="chatrooms"/>
-        <IconButton icon={settingsIcon} page="settings"/>
-        <IconButton icon={logoutIcon} page="login"/>
-	    </div> 
-    </div>
+        <div className='w-full h-50'>
+            <div className='flex w-full h-full justify-evenly  bg-greenflag-green'>
+            <IconButton icon={matchingIcon} page="matching"/>
+            <IconButton icon={messageIcon} page="chatrooms"/>
+            <IconButton icon={settingsIcon} page="settings"/>
+            <IconButton icon={logoutIcon} page="login"/>
+            </div> 
+        </div>
     );
 
 };
