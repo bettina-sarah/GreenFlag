dfe463c2add68d524fa20abc123c40d688cddee8
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-11-11 12:05:33 -0500

    refactor get id in progress

--- dev/frontend/src/components/LoginForm.tsx
+++ dev/frontend/src/components/LoginForm.tsx
@@ -25,7 +25,7 @@ const LoginForm = () => {
             const answer = await axios.post(IP_SERVER+'/login',data)
             if (answer.data){
                 console.log(answer);
-                sessionStorage.setItem("email", data.email);
+                sessionStorage.setItem("id", data.email);
                 if(localStorage.getItem("fillQuestionnaire") === "true"){
                     navigate('/questionnaire')
                 }
