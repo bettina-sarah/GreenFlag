6908782761845a77c27a8c9b55444a5cd010a199
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-11-11 11:48:57 -0500

    refactor create account & navigation

--- dev/frontend/src/pages/Login.tsx
+++ dev/frontend/src/pages/Login.tsx
@@ -1,14 +1,31 @@
 import LoginForm from '@/components/LoginForm';
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-const Login: React.FC = () => (
+
+const Login: React.FC = () => {
+
+	const [showAccountCreatedMessage, setshowAccountCreatedMessage] = useState(false);
+
+	 if (localStorage.getItem('accountCreated') === 'true' && !showAccountCreatedMessage) {
+		setshowAccountCreatedMessage(true);
+		localStorage.removeItem('accountCreated');
+		localStorage.setItem('fillQuestionnaire', 'true');
+	  }
+
+	return(
 	<div className='w-full h-full flex flex-col justify-between items-center'>
 		<h1 className='text-3xl font-bold text-teal-500'>Welcome Back !</h1>
+		{showAccountCreatedMessage && (
+        <div className="alert alert-success">
+          Account created successfully! Please log in.
+        </div>
+      )}
 		<div className='flex flex-col m-2 py-3'>
 			<LoginForm />
 		</div>
 	</div>
-);
+	);
+};
 
 export default Login;
