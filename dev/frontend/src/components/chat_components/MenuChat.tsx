64246f5e72592f44e1fd35f46d6a8db3ea8615ca
Vincent <genesisqc@hotmail.ca>
2024-11-11 12:36:26 -0500

    ChatMenu changes

--- dev/frontend/src/components/chat_components/MenuChat.tsx
+++ dev/frontend/src/components/chat_components/MenuChat.tsx
@@ -1,10 +1,9 @@
-import React, {useEffect} from 'react';
-import IconButton from '../IconButton';
+import React, {useEffect,useState} from 'react';
+import IconButton from './IconButton';
 import matchingIcon from '../../ressources/icons/matching.png'
-import messageIcon from '../../ressources/icons/messages.png'
-import settingsIcon from '../../ressources/icons/settings.png'
-import logoutIcon from '../../ressources/icons/logout.png'
+import backIcon from '../../../ressources/icons/back_arrow.png'
 import useFetch from '@/api/useFetch';
+import { Avatar } from 'flowbite-react';
 
 interface MenuChatProp{
     subject_id: number;
@@ -12,6 +11,7 @@ interface MenuChatProp{
 
 interface Subject{
     id: string;
+
 }
 
 interface IPhotoData {
@@ -31,6 +31,8 @@ const MenuChat: React.FC<MenuChatProp> = ({subject_id}) => {
         data: { id: subject_id },
     });
 
+    const [photo,setPhoto] = useState<IPhotoData>()
+
     useEffect(() => {
         if (subjectData && !subjectLoading && !subjectError) {
             const photoKeys = subjectData?.user_infos?.photo_keys;
@@ -42,10 +44,8 @@ const MenuChat: React.FC<MenuChatProp> = ({subject_id}) => {
     return(
         <div className='w-full h-50'>
             <div className='flex w-full h-full justify-evenly  bg-greenflag-green'>
-            <IconButton icon={matchingIcon} page="matching"/>
-            <IconButton icon={messageIcon} page="chatrooms"/>
-            <IconButton icon={settingsIcon} page="settings"/>
-            <IconButton icon={logoutIcon} page="login"/>
+                <IconButton icon={backIcon} page="chatrooms"/>
+                <Avatar img={}/>
             </div> 
         </div>
     );
