cd080df2f024d5b832e400928be0b7d40f731233
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-11-11 11:19:11 -0500

    frontend refactor HomePage

--- dev/frontend/src/pages/HomePage.tsx
+++ dev/frontend/src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import Logo from '../../ressources/icons/logo.png'
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate();
@@ -10,22 +11,12 @@ const HomePage: React.FC = () => {
     navigate('/create-account');
   };
 
-  const handleButtonClickTest = () => {
-    navigate('/test');
-  };
-
-  const handleButtonClickSettings = () => {
-    navigate('/settings');
-  };
-
-  
-  const handleButtonClickProfile = () => {
-    navigate('/profile');
-  };
-
   return (
     <div className='w-full h-full flex flex-col justify-between items-center'>
-      <h1 className='text-3xl font-bold text-teal-500'>GreenFlag</h1>
+      <h1 className='p-8 text-3xl font-extrabold text-teal-500'>GreenFlag</h1>
+      <div>
+        <img src={Logo} alt="" />
+      </div>
       <div className='flex flex-col justify-between'>
         <h2 className='text-align-left text-2xl'>Emotional availability-based matching</h2>
         <p>Tired of traditional dating apps? We got you!</p>
@@ -35,15 +26,6 @@ const HomePage: React.FC = () => {
           </button>
           <button onClick={handleRegisterButtonClick} className='bg-teal-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2'>
             Create an Account
-          </button>
-          <button onClick={handleButtonClickTest} className='bg-teal-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2'>
-            Go to backend test!
-          </button>
-          <button onClick={handleButtonClickSettings} className='bg-pink-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2'>
-            Settings page
-          </button>
-          <button onClick={handleButtonClickProfile} className='bg-purple-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2'>
-            My profile
           </button>
         </div>
       </div>
