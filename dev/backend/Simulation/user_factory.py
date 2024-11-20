96fa43cc9d9a131fdb764befb5fe4e7bbf4fce04
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-11-20 13:04:11 -0500

    factory activities array numpy functional

--- dev/backend/Simulation/user_factory.py
+++ dev/backend/Simulation/user_factory.py
@@ -1,9 +1,9 @@
-from user import User
 from __future__ import annotations
+from user import User
 from abc import ABC, abstractmethod
 from faker import Faker
-import random
-import copy
+# import random
+# import copy
 import numpy as np
 
 
@@ -29,28 +29,28 @@ ACTIVITY_TUPLE = ("hiking",
 "learningnewlanguage"
 )
 
-ACTIVITY_DICT = {activity: False for activity in ACTIVITY_TUPLE}
+# ACTIVITY_DICT = {activity: False for activity in ACTIVITY_TUPLE}
 
 
 class Factory(ABC):
     def __init__(self) -> None:
-        super().__init__()
-    
-    @abstractmethod
-    def factory_method_men(self):
         pass
+    
+    # @abstractmethod
+    # def factory_method_men(self):
+    #     pass
 
     @abstractmethod
     def factory_method_young_men(self):
         pass
     
-    @abstractmethod
-    def factory_method_women(self):
-        pass
+    # @abstractmethod
+    # def factory_method_women(self):
+    #     pass
     
-    @abstractmethod
-    def factory_method_nonbinary(self):
-        pass
+    # @abstractmethod
+    # def factory_method_nonbinary(self):
+    #     pass
 
 class UserFactory(Factory):
     def __init__(self, name) -> None:
@@ -58,34 +58,34 @@ class UserFactory(Factory):
         self.__name = name
 
     def factory_method_young_men(self):
-        # activity_set = set()
         
-        
-        '''np full
-        0-5 true et shuffe
-        zip
         '''
+        SET & DICTIONNARY WAY
+        '''
+        # activity_set = set()
         # while len(activity_set) < 5:
         #     activity = random.randint(0,19)
         #     activity_set.add(ACTIVITY_TUPLE[activity])
-            
         # new_dict = copy.deepcopy(ACTIVITY_DICT)
-        
         # for activity in activity_set:
         #     new_dict[activity] = True
-        
+
+        '''
+        NUMPY WAY
+        '''
         activity_list = np.full(20,False,bool)
-        
+        activity_list[0:5] = True
+        np.random.shuffle(activity_list)
+        zipped = zip(ACTIVITY_TUPLE,activity_list)
+        activity_dict = dict(zipped)
         
 
-        
-                
-            
             
                 
-        user = User(self.__faker.name_male, self.__faker.date_of_birth(minimum_age=18,maximum_age=35), gender='Male', preferences=[],interests=activity_set)
-        self.__faker.name_nonbinary
-        self.__faker.name_male
+        user = User(self.__faker.name_male, self.__faker.date_of_birth(minimum_age=18,maximum_age=35),
+                     gender='Male', preferences=[],interests=activity_dict)
+        # self.__faker.name_nonbinary
+        # self.__faker.name_male
         # make women, non binary, males that have preferences also: 
         
     # old men, young old women, young old nb
@@ -98,4 +98,5 @@ class UserFactory(Factory):
         pass
 
 if __name__ == "__main__":
-    pass
+    user_factory = UserFactory("men")
+    user_factory.factory_method_young_men()
