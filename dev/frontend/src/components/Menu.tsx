86065852d64d56a716c0ab94f3d089db6986eb0c
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-11-20 21:11:27 -0500

    implemented dropdown for notification bell, 50% functional

--- dev/frontend/src/components/Menu.tsx
+++ dev/frontend/src/components/Menu.tsx
@@ -6,8 +6,16 @@ import settingsIcon from "../../ressources/icons/settings.png";
 import logoutIcon from "../../ressources/icons/logout.png";
 import bellIcon from "../../ressources/icons/bell_notification.png";
 import ThemeSelector from "./ThemeSelector";
+import { useState } from "react";
+import Notification from "./Notification";
 
 const Menu: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div className="w-full h-50">
       <div className="flex w-full h-full justify-evenly  bg-greenflag-green">
@@ -15,13 +23,16 @@ const Menu: React.FC = () => {
         <IconButton icon={messageIcon} page="chatrooms" />
         {/* <ThemeSelector/> */}
         <IconButton icon={settingsIcon} page="settings" />
-        <IconButton icon={bellIcon} page="settings" />
+        <IconButton icon={bellIcon} onClick={toggleDropdown} />
+        {isVisible && <Notification />}
+
         <IconButton
           icon={logoutIcon}
           onClick={() => {
             sessionStorage.clear();
             localStorage.clear();
             //function to clear & post to BE 1st then clear
+            // post to remove token from database!
           }}
           page="login"
         />
