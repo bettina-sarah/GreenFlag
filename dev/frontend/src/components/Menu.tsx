60fc56392ea78ef88bc095501e0f0554638786d3
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-11-20 21:45:09 -0500

    notification drop down component refactor

--- dev/frontend/src/components/Menu.tsx
+++ dev/frontend/src/components/Menu.tsx
@@ -4,11 +4,8 @@ import matchingIcon from "../../ressources/icons/matching.png";
 import messageIcon from "../../ressources/icons/messages.png";
 import settingsIcon from "../../ressources/icons/settings.png";
 import logoutIcon from "../../ressources/icons/logout.png";
-import bellIcon from "../../ressources/icons/bell_notification.png";
 import ThemeSelector from "./ThemeSelector";
-import { useState } from "react";
-import Notification from "./Notification";
-import { Dropdown } from "flowbite-react";
+import NotificationDropDown from "./NotificationDropDown";
 
 const Menu: React.FC = () => {
   // const [isVisible, setIsVisible] = useState(false);
@@ -24,18 +21,7 @@ const Menu: React.FC = () => {
         <IconButton icon={messageIcon} page="chatrooms" />
         {/* <ThemeSelector/> */}
         <IconButton icon={settingsIcon} page="settings" />
-        <Dropdown inline label={<IconButton icon={bellIcon} />}>
-          <Dropdown.Item onClick={() => alert("Dashboard!")}>
-            Dashboard
-          </Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-          <Dropdown.Item as="a" href="https://flowbite.com/" target="_blank">
-            External link
-          </Dropdown.Item>
-        </Dropdown>
+        <NotificationDropDown />
 
         <IconButton
           icon={logoutIcon}
