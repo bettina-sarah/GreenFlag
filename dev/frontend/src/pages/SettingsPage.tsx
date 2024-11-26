835044f459cc0586e0b96430f729bc460ed81e3c
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-11-26 10:22:47 -0500

    notification dropdown beautification

--- dev/frontend/src/pages/SettingsPage.tsx
+++ dev/frontend/src/pages/SettingsPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import DeleteAccountForm from '@/components/form_components/DeleteAccountForm';
+import Menu from '@/components/Menu';
 
 const SettingsPage: React.FC = () => (
 	<div className='w-full h-full flex flex-col justify-between items-center'>
+		<Menu />
 		<h1 className='text-3xl font-bold text-teal-500'>Delete account !</h1>
 		<div className='flex flex-col m-2 py-3'>
 			<DeleteAccountForm />
