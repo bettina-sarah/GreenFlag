59a771710eb0649671b340aeba27cc18cd652877
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-11-26 10:47:19 -0500

    logout button refactor

--- dev/frontend/src/pages/SettingsPage.tsx
+++ dev/frontend/src/pages/SettingsPage.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import DeleteAccountForm from '@/components/form_components/DeleteAccountForm';
 import Menu from '@/components/Menu';
+import IconButton from '@/components/IconButton';
+import logoutIcon from "../../ressources/icons/logout.png";
 
-const SettingsPage: React.FC = () => (
+const SettingsPage: React.FC = () => {
+	return(
 	<div className='w-full h-full flex flex-col justify-between items-center'>
 		<Menu />
+		<h1>Logout</h1>
+		
+        <IconButton
+          icon={logoutIcon}
+          onClick={() => {
+            sessionStorage.clear();
+            localStorage.clear();
+            //function to clear & post to BE 1st then clear
+            // post to remove token from database!
+          }}
+          page="login"
+        />
 		<h1 className='text-3xl font-bold text-teal-500'>Delete account !</h1>
 		<div className='flex flex-col m-2 py-3'>
 			<DeleteAccountForm />
 		</div>
 	</div>
-);
+	);
+};
 
 export default SettingsPage;
