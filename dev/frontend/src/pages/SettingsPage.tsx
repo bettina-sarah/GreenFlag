58c5004c7002a06a5e4668427e3e6c7c19b4a970
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-11-27 20:47:23 -0500

    questionnaire added to settings page

--- dev/frontend/src/pages/SettingsPage.tsx
+++ dev/frontend/src/pages/SettingsPage.tsx
@@ -1,33 +1,42 @@
-import React from 'react';
-import DeleteAccountForm from '@/components/form_components/DeleteAccountForm';
-import Menu from '@/components/Menu';
-import { NotificationProvider } from '@/components/NotificationContext';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import DeleteAccountForm from "@/components/form_components/DeleteAccountForm";
+import Menu from "@/components/Menu";
+import { NotificationProvider } from "@/components/NotificationContext";
+import { useNavigate } from "react-router-dom";
+import QuestionnaireForm from "@/components/form_components/QuestionnaireForm";
 
 const SettingsPage: React.FC = () => {
   const navigate = useNavigate();
-	return(
-	<div className='w-full h-full flex flex-col justify-between items-center'>
-		  <NotificationProvider>
+  return (
+    <div className="w-full h-full flex flex-col justify-between items-center">
+      <NotificationProvider>
         <Menu />
       </NotificationProvider>
-		<h1>Logout</h1>
-    <button onClick={() => {
-            sessionStorage.clear();
-            navigate("/");
+      <h1>Modify your profile</h1>
+      <div className="flex-grow-1 flex flex-col justify-evenly items-center">
+        <h1 className="text-3xl font-bold text-teal-500">Questionnaire</h1>
+        <QuestionnaireForm />
+      </div>
+      <h1>Logout</h1>
+      <button
+        onClick={() => {
+          sessionStorage.clear();
+          navigate("/");
 
-            //function to clear & post to BE 1st then clear
-            // post to remove token from database!
-          }}
-           className="bg-red-600 p-1 rounded-md text-white" type="submit">
-            Logout    
+          //function to clear & post to BE 1st then clear
+          // post to remove token from database!
+        }}
+        className="bg-red-600 p-1 rounded-md text-white"
+        type="submit"
+      >
+        Logout
       </button>
-		<h1 className='text-3xl font-bold text-teal-500'>Delete account !</h1>
-		<div className='flex flex-col m-2 py-3'>
-			<DeleteAccountForm />
-		</div>
-	</div>
-	);
+      <h1 className="text-3xl font-bold text-teal-500">Delete account !</h1>
+      <div className="flex flex-col m-2 py-3">
+        <DeleteAccountForm />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsPage;
