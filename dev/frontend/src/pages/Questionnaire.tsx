2d06b27c4ff3febd39e3b563b6ff63e5f2233b53
Vincent Fournier <genesisqc@hotmail.ca>
2024-11-28 00:10:52 -0500

    questionnaire half themed

--- dev/frontend/src/pages/Questionnaire.tsx
+++ dev/frontend/src/pages/Questionnaire.tsx
@@ -5,7 +5,7 @@ import QuestionnaireForm from "@/components/form_components/QuestionnaireForm";
 const QuestionnairePage: React.FC = () => {
   return (
     <div className="flex-grow-1 flex flex-col justify-evenly items-center">
-      <h1 className="text-3xl font-bold text-teal-500">Questionnaire</h1>
+      <h1 className="m-5 text-4xl font-bold text-base-text font-leckerli">Questionnaire</h1>
       <QuestionnaireForm />
     </div>
   );
