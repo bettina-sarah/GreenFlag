3c250c185befc77837351677ff9d4e7c9886c596
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-12-03 11:06:29 -0500

    successful user creation

--- dev/backend/Simulation/user_factory.py
+++ dev/backend/Simulation/user_factory.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
-from user import User
+from Simulation.user import User
 from abc import ABC, abstractmethod
 from faker import Faker
 import random
 import numpy as np
 from DAOs.photo_lmdb_dao import PhotoDAO
+from Managers.account_manager import AccountManager
+from typing import List
 
 
 ACTIVITY_TUPLE = ("hiking",
@@ -100,10 +102,10 @@ class UserFactory(Factory):
 
     def factory_method_young_men(self):
         
-        {'id': '1', 'info': {'gender': 'Male', 'height': '185', 'religion': 'Jewish', 'want_kids': True, 
-                             'city': 'Montreal', 'bio': 'dfavsdf', 'min_age': '29', 'max_age': '60', 
-                             'relationship_type': 'shortterm', 'date_of_birth': '2005-02-01T05:00:00.000Z', 
-                             'preferred_genders': ['Female']}}    
+        # {'id': '1', 'info': {'gender': 'Male', 'height': '185', 'religion': 'Jewish', 'want_kids': True, 
+        #                      'city': 'Montreal', 'bio': 'dfavsdf', 'min_age': '29', 'max_age': '60', 
+        #                      'relationship_type': 'shortterm', 'date_of_birth': '2005-02-01T05:00:00.000Z', 
+        #                      'preferred_genders': ['Female']}}    
         # & activties
 
         activity_dict = self.generate_activities()
@@ -111,8 +113,8 @@ class UserFactory(Factory):
     
         photoDAO = PhotoDAO()
         # password by default
-        user = User(self.__faker.first_name_male, self.__faker.last_name_male, self.__faker.date_of_birth(minimum_age=18,maximum_age=35),
-                     gender='Male', email=self.__faker.email(),preferences=preferences_dict,interests=activity_dict, bio = self.__faker.text(200), photo_key=)
+        user = User(self.__faker.first_name_male(), self.__faker.last_name_male(), self.__faker.date_of_birth(minimum_age=18,maximum_age=35),
+                     gender='Male', email=self.__faker.email(),preferences=preferences_dict,interests=activity_dict, bio = self.__faker.text(200), photo_key=photoDAO.add_photos())
         
         return user
         # self.__faker.name_nonbinary
@@ -120,6 +122,21 @@ class UserFactory(Factory):
         # make women, non binary, males that have preferences also: 
         
     # old men, young old women, young old nb
+
+    def add_to_database(self, user_list: list[User]):
+        for user in user_list:
+            user_id = AccountManager.create_account(user.basic_account_info)
+            # {'id': '1', 'hobbies': {'hiking': False, 'yoga': False, 'photography': False, 'cooking': False, 
+            #                         'traveling': False, 'reading': False, 'videogaming': False, 'biking': False, 'running': False, 
+            #                         'watchingmovies': False, 'workingout': False, 'dancing': False, 'playinginstrument': False, 'attendingconcerts': True, 
+            #                         'painting': True, 'volunteering': True, 'playingsports': True, 'crafting': True, 'petlover': False, 'learningnewlanguage': False}}
+            
+            
+            
+            AccountManager.complete_profile({'id': user_id})
+            
+        
+        
     
 
 if __name__ == "__main__":
--- dev/backend/server.py
+++ dev/backend/server.py
@@ -181,8 +181,11 @@ def update_notification() -> bool:
     print('response db is: ', response)
     return jsonify(response)
 
-
+from Simulation.user_factory import UserFactory
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    user_factory = UserFactory("men")
+    user = user_factory.factory_method_young_men()
+    pass
+    # socketio.run(app, debug=True, host="0.0.0.0", port=5000)
     #app.run(debug=True, host="0.0.0.0", port=5000)
