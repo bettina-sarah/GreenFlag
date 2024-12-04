e8e02aaf9127df6c6644f2bff0ddec29c88ef505
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-12-04 16:54:20 -0500

    functional factory git add .!

--- dev/backend/Simulation/test_simulator.py
+++ dev/backend/Simulation/test_simulator.py
@@ -1,4 +1,4 @@
-from user_factory import UserFactory
+from Simulation.user_factory import UserFactory
 import random
 
 class TestSimulator:
@@ -8,9 +8,9 @@ class TestSimulator:
 
     def create_random_users(self, user_amount: int= 100, gender_proportion:list = [0.25,0.25,0.25,0.25]):
         random.shuffle(gender_proportion)       
-        for gender, index in enumerate(gender_proportion):
-            for i in range(user_amount*gender):
-                user = self.user_factory.factory_method(gender=UserFactory.GENDER[index],age_type=UserFactory.AGE_TYPE(i % 3))
+        for index, gender in enumerate(gender_proportion):
+            for i in range(int(user_amount*gender)):
+                user = self.user_factory.factory_method(gender=UserFactory.GENDER[index],age_type=UserFactory.AGE_TYPE[i % 3])
                 self.user_factory.add_to_database(user)
                 self.users.add(user)
 
