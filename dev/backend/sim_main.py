29ce078a160f1a49382aab6695d26673a4f8e1cb
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-12-13 09:59:30 -0500

    logger refactor aagin

--- dev/backend/sim_main.py
+++ dev/backend/sim_main.py
@@ -1,6 +1,21 @@
 
 from Simulation.test_simulator import TestSimulator
 
+import logging
+import coloredlogs
+
+level_styles = {
+    'debug': {'color': 'blue'},
+    'info': {'color': 'green'},
+    'warning': {'color': 'yellow'},
+    'error': {'color': 'red'},
+    'critical': {'color': 'magenta'}
+}
+
+
+coloredlogs.install(level='DEBUG', level_styles=level_styles)
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     sim = TestSimulator()
     sim.create_random_users(200)
