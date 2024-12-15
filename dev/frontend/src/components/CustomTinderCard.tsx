aa94827920b0d57a58f3565af8531110ec039caa
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-12-14 20:43:08 -0500

    card shadown

--- dev/frontend/src/components/CustomTinderCard.tsx
+++ dev/frontend/src/components/CustomTinderCard.tsx
@@ -77,8 +77,11 @@ const CustomTinderCard: React.FC<IProfileProps> = ({
         onCardLeftScreen={handleCardLeftScreen}
       >
         {/* <div className="w-96 bg-primary-color p-1 rounded-3xl relative"> */}
-        <div className="min-w-[48vw] max-w-[96vw]    lg:min-w-[23vw] lg:max-w-[24vw] bg-primary-color p-2 rounded-3xl relative min-h-[90vh] max-h-[95vh] lg:min-h-[95vh] lg:max-h-[100vh] overflow-hidden">
-          <PhotoCarousel images={photos} />
+        <div className=" shadow-2xl shadow-slate-950 min-w-[48vw] max-w-[96vw]    lg:min-w-[23vw] lg:max-w-[24vw] bg-primary-color p-2 rounded-3xl relative min-h-[90vh] max-h-[95vh] lg:min-h-[95vh] lg:max-h-[100vh] overflow-hidden">
+          <div className="pt-1">
+            <PhotoCarousel images={photos} />
+          </div>
+
           <BasicInfo basic_info={profile_info.basic_info} />
           <RelationshipGoals
             relationship={profile_info.relationship}
