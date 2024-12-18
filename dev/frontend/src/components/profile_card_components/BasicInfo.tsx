cf7c9e966b447a1839e0767b8b2010ccffa6608d
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-12-18 11:42:03 -0500

    frontend location done

--- dev/frontend/src/components/profile_card_components/BasicInfo.tsx
+++ dev/frontend/src/components/profile_card_components/BasicInfo.tsx
@@ -1,3 +1,4 @@
+import useFetch from "@/api/useFetch";
 import React from "react";
 
 interface BasicInfoProps {
@@ -5,11 +6,40 @@ interface BasicInfoProps {
     first_name: string;
     age: number;
     city: string;
-    location: number;
   };
 }
 
+interface LocationData {
+  location: string;
+}
+
+
 const BasicInfo: React.FC<BasicInfoProps> = ({ basic_info }) => {
+
+
+  const {
+    data: locationData,
+    loading: locationLoading,
+    error: locationError,
+  } = useFetch<LocationData>({
+    url: "/get-location",
+    data: { id: sessionStorage.getItem("id") },
+  });
+
+  if (!locationData && locationLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!locationData && !locationLoading && locationError) {
+    return (
+      <div>
+        <div>Error: {locationError}</div>
+      </div>
+    );
+  }
+
+
+
   return (
     <div className="flex flex-col items-baseline pl-4 pt-1 mt-2 mb-2">
       <h1 className="font-nunito-extrabold text-h1-custom mb-1 ">
@@ -19,7 +49,7 @@ const BasicInfo: React.FC<BasicInfoProps> = ({ basic_info }) => {
         Lives in {basic_info.city}
       </h2>
       <h2 className="font-nunito-semibold text-muted-text">
-        {basic_info.location} km away
+        {locationData?.location} km away
       </h2>
     </div>
   );
