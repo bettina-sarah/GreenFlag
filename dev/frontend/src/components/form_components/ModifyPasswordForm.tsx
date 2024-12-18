3b93039d9eb9796bf1a9de4337112ecbd60240e3
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-12-17 22:37:44 -0500

    acct settings buttons, titles

--- dev/frontend/src/components/form_components/ModifyPasswordForm.tsx
+++ dev/frontend/src/components/form_components/ModifyPasswordForm.tsx
@@ -3,15 +3,15 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import LockIcon from "@/../ressources/icons/lock.png";
 import EmailIcon from "@/../ressources/icons/email.png";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 const ModifyPasswordForm = () => {
   type FormData = {
     email: string;
     password: string;
     cpassword: string;
-    newpassword:string;
-    cnewpassword:string;
+    newpassword: string;
+    cnewpassword: string;
   };
 
   const email = sessionStorage.getItem("email") || "";
@@ -23,14 +23,14 @@ const ModifyPasswordForm = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const PasswordValue = watch('password');
-  const NewPasswordValue = watch('newpassword');
+  const PasswordValue = watch("password");
+  const NewPasswordValue = watch("newpassword");
 
   const onSubmit = handleSubmit(async (data: any) => {
     try {
       const answer = await axios.post(IP_SERVER + "/modify-password", data);
       if (answer.data) {
-        toast.success('Password succesfully changed');
+        toast.success("Password succesfully changed");
       }
     } catch (error) {
       console.error("Error during password modification:", error);
@@ -38,60 +38,115 @@ const ModifyPasswordForm = () => {
   });
 
   return (
-    <form onSubmit={onSubmit} className="flex flex-col h-60 justify-between m-5">
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col h-60 justify-between m-5"
+    >
       <div className="flex items-center w-full max-w-sm border-b-2 h-6 border-custom-bg">
-        <img src={EmailIcon} className="size-7"/>
-        <input className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
-        placeholder="Email"
-        defaultValue={email}
-        {...register("email", { required: true, pattern: /^[\w.]+@([\w-]+\.)+[\w-]{2,4}$/ })}/>
+        <img src={EmailIcon} className="size-7" />
+        <input
+          className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
+          placeholder="Email"
+          defaultValue={email}
+          {...register("email", {
+            required: true,
+            pattern: /^[\w.]+@([\w-]+\.)+[\w-]{2,4}$/,
+          })}
+        />
       </div>
-        {errors.email?.type === "required" && <span className="text-red-500 text-xs">This is required</span>}
-        {errors.email?.type === "pattern" && <span className="text-red-500 text-xs">You need to provide a valid email</span>}
+      {errors.email?.type === "required" && (
+        <span className="text-red-500 text-xs">This is required</span>
+      )}
+      {errors.email?.type === "pattern" && (
+        <span className="text-red-500 text-xs">
+          You need to provide a valid email
+        </span>
+      )}
 
       {/* Old Password */}
       <div className="flex items-center w-full max-w-sm border-b-2 h-6 border-custom-bg">
-        <img src={LockIcon} className="size-7"/>
-        <input className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
-        placeholder="Old password"
-        type="password"
-        {...register("password", { required: true, maxLength: 20 })}/>
+        <img src={LockIcon} className="size-7" />
+        <input
+          className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
+          placeholder="Old password"
+          type="password"
+          {...register("password", { required: true, maxLength: 20 })}
+        />
       </div>
-        {errors.password?.type === "required" && <span className="text-red-500 text-xs">This is required</span>}
-        {errors.password?.type === "maxLength" && <span className="text-red-500 text-xs">Max length exceeded</span>}
+      {errors.password?.type === "required" && (
+        <span className="text-red-500 text-xs">This is required</span>
+      )}
+      {errors.password?.type === "maxLength" && (
+        <span className="text-red-500 text-xs">Max length exceeded</span>
+      )}
 
       <div className="flex items-center w-full max-w-sm border-b-2 h-6 border-custom-bg">
-        <img src={LockIcon} className="size-7"/>
-        <input className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
-        placeholder="Old password confirmation"
-        type="password"
-        {...register("cpassword", { required: true, validate: (val:string) =>PasswordValue != val ? "Your passwords do not match" : true, })}/>
+        <img src={LockIcon} className="size-7" />
+        <input
+          className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
+          placeholder="Old password confirmation"
+          type="password"
+          {...register("cpassword", {
+            required: true,
+            validate: (val: string) =>
+              PasswordValue != val ? "Your passwords do not match" : true,
+          })}
+        />
       </div>
-        {errors.password?.type === "required" && <span className="text-red-500 text-xs">This is required</span>}
-        {errors.cpassword?.message && <span className="text-red-500 text-xs">{errors.cpassword?.message}</span>}
+      {errors.password?.type === "required" && (
+        <span className="text-red-500 text-xs">This is required</span>
+      )}
+      {errors.cpassword?.message && (
+        <span className="text-red-500 text-xs">
+          {errors.cpassword?.message}
+        </span>
+      )}
 
       {/* New Password */}
       <div className="flex items-center w-full max-w-sm border-b-2 h-6 border-custom-bg">
-        <img src={LockIcon} className="size-7"/>
-        <input className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
-        placeholder="New password"
-        type="password"
-        {...register("newpassword", { required: true, maxLength: 20 })}/>
+        <img src={LockIcon} className="size-7" />
+        <input
+          className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
+          placeholder="New password"
+          type="password"
+          {...register("newpassword", { required: true, maxLength: 20 })}
+        />
       </div>
-        {errors.password?.type === "required" && <span className="text-red-500 text-xs">This is required</span>}
-        {errors.password?.type === "maxLength" && <span className="text-red-500 text-xs">Max length exceeded</span>}
+      {errors.password?.type === "required" && (
+        <span className="text-red-500 text-xs">This is required</span>
+      )}
+      {errors.password?.type === "maxLength" && (
+        <span className="text-red-500 text-xs">Max length exceeded</span>
+      )}
 
       <div className="flex items-center w-full max-w-sm border-b-2 h-6 border-custom-bg">
-        <img src={LockIcon} className="size-7"/>
-        <input className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
-        placeholder="New password confirmation"
-        type="password"
-        {...register("cnewpassword", { required: true, validate: (val:string) =>NewPasswordValue != val ? "Your passwords do not match" : true, })}/>
+        <img src={LockIcon} className="size-7" />
+        <input
+          className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
+          placeholder="New password confirmation"
+          type="password"
+          {...register("cnewpassword", {
+            required: true,
+            validate: (val: string) =>
+              NewPasswordValue != val ? "Your passwords do not match" : true,
+          })}
+        />
       </div>
-        {errors.password?.type === "required" && <span className="text-red-500 text-xs">This is required</span>}
-        {errors.cpassword?.message && <span className="text-red-500 text-xs">{errors.cpassword?.message}</span>}
+      {errors.password?.type === "required" && (
+        <span className="text-red-500 text-xs">This is required</span>
+      )}
+      {errors.cpassword?.message && (
+        <span className="text-red-500 text-xs">
+          {errors.cpassword?.message}
+        </span>
+      )}
 
-      <button className="bg-secondary-color p-1 rounded-md text-white" type="submit">
+      <button
+        className="transition-colors duration-300 bg-secondary-color hover:bg-primary-color
+             text-custom-bg/85 hover:text-custom-bg border-2 border-secondary-color
+             font-bold py-2 px-4 rounded my-4 mx-6 w-72"
+        type="submit"
+      >
         Change Password
       </button>
     </form>
