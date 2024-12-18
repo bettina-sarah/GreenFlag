3a89080ff4e42adc5042f7f056f029dc9db92104
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2024-12-17 22:37:44 -0500

    acct settings buttons, titles

--- dev/frontend/src/components/form_components/DeleteAccountForm.tsx
+++ dev/frontend/src/components/form_components/DeleteAccountForm.tsx
@@ -6,7 +6,6 @@ import axios from "axios";
 import LockIcon from "@/../ressources/icons/lock.png";
 import EmailIcon from "@/../ressources/icons/email.png";
 
-
 const DeleteAccountForm = () => {
   const navigate = useNavigate();
   type FormData = {
@@ -22,7 +21,7 @@ const DeleteAccountForm = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-  const PasswordValue = watch('password');
+  const PasswordValue = watch("password");
 
   const onSubmit = handleSubmit(async (data: FormData) => {
     try {
@@ -31,7 +30,7 @@ const DeleteAccountForm = () => {
         console.log(answer);
         console.log("Account deleted successfully");
         navigate("/login");
-        toast.success("Account deleted successfully")
+        toast.success("Account deleted successfully");
       }
     } catch (error) {
       console.error("Error during login:", error);
@@ -39,37 +38,74 @@ const DeleteAccountForm = () => {
   });
 
   return (
-    <form onSubmit={onSubmit} className="flex flex-col h-48 justify-between m-5">
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col h-48 justify-between m-5"
+    >
       <div className="flex items-center w-full max-w-sm border-b-2 h-6 border-custom-bg">
-        <img src={EmailIcon} className="size-7"/>
-        <input className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
-        placeholder="Email"
-        {...register("email", { required: true, pattern: /^[\w.]+@([\w-]+\.)+[\w-]{2,4}$/ })}/>
+        <img src={EmailIcon} className="size-7" />
+        <input
+          className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
+          placeholder="Email"
+          {...register("email", {
+            required: true,
+            pattern: /^[\w.]+@([\w-]+\.)+[\w-]{2,4}$/,
+          })}
+        />
       </div>
-        {errors.email?.type === "required" && <span className="text-red-500 text-xs">This is required</span>}
-        {errors.email?.type === "pattern" && <span className="text-red-500 text-xs">You need to provide a valid email</span>}
+      {errors.email?.type === "required" && (
+        <span className="text-red-500 text-xs">This is required</span>
+      )}
+      {errors.email?.type === "pattern" && (
+        <span className="text-red-500 text-xs">
+          You need to provide a valid email
+        </span>
+      )}
 
       <div className="flex items-center w-full max-w-sm border-b-2 h-6 border-custom-bg">
-        <img src={LockIcon} className="size-7"/>
-        <input className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
-        placeholder="Password"
-        type="password"
-        {...register("password", { required: true, maxLength: 20 })}/>
+        <img src={LockIcon} className="size-7" />
+        <input
+          className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
+          placeholder="Password"
+          type="password"
+          {...register("password", { required: true, maxLength: 20 })}
+        />
       </div>
-        {errors.password?.type === "required" && <span className="text-red-500 text-xs">This is required</span>}
-        {errors.password?.type === "maxLength" && <span className="text-red-500 text-xs">Max length exceeded</span>}
+      {errors.password?.type === "required" && (
+        <span className="text-red-500 text-xs">This is required</span>
+      )}
+      {errors.password?.type === "maxLength" && (
+        <span className="text-red-500 text-xs">Max length exceeded</span>
+      )}
 
       <div className="flex items-center w-full max-w-sm border-b-2 h-6 border-custom-bg">
-        <img src={LockIcon} className="size-7"/>
-        <input className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
-        placeholder="Password confirmation"
-        type="password"
-        {...register("cpassword", { required: true, validate: (val:string) =>PasswordValue != val ? "Your passwords do not match" : true, })}/>
+        <img src={LockIcon} className="size-7" />
+        <input
+          className="pl-3 w-80 text-custom-bg font-inter bg-transparent border-none focus:outline-none focus:ring-0 placeholder:text-custom-bg"
+          placeholder="Password confirmation"
+          type="password"
+          {...register("cpassword", {
+            required: true,
+            validate: (val: string) =>
+              PasswordValue != val ? "Your passwords do not match" : true,
+          })}
+        />
       </div>
-        {errors.password?.type === "required" && <span className="text-red-500 text-xs">This is required</span>}
-        {errors.cpassword?.message && <span className="text-red-500 text-xs">{errors.cpassword?.message}</span>}
+      {errors.password?.type === "required" && (
+        <span className="text-red-500 text-xs">This is required</span>
+      )}
+      {errors.cpassword?.message && (
+        <span className="text-red-500 text-xs">
+          {errors.cpassword?.message}
+        </span>
+      )}
 
-      <button className="bg-red-600 p-1 rounded-md text-white" type="submit">
+      <button
+        className="transition-colors duration-300 bg-red-600 hover:bg-primary-color
+             text-red-900 hover:text-custom-bg border-2 border-red-950
+             font-bold py-2 px-4 rounded my-4 mx-6 w-72"
+        type="submit"
+      >
         Delete Account
       </button>
     </form>
--- dev/frontend/src/pages/AccountSettingsPage.tsx
+++ dev/frontend/src/pages/AccountSettingsPage.tsx
@@ -5,28 +5,35 @@ import { NotificationProvider } from "@/components/NotificationContext";
 import ModifyPasswordForm from "@/components/form_components/ModifyPasswordForm";
 import AlgoSelector from "@/components/AlgoSelector";
 
-
 const AccountSettingsPage: React.FC = () => {
-
   return (
-    <div className="w-full h-full flex flex-col justify-between items-center">
+    <div className="w-full flex flex-col justify-between items-center bg-primary-color">
       <NotificationProvider>
         <Menu />
       </NotificationProvider>
 
       <div className="flex flex-col justify-around items-center m-6 h-80 bg-primary-color rounded-3xl">
-        <h1 className="pt-3 text-3xl max-w-sm font-bold text-base-text">Choose an assistant to help you find love</h1>
-        <AlgoSelector/>
-      </div>
+        <h1 className="m-3 py-4 text-5xl font-bold text-base-text font-leckerli">
+          Account settings
+        </h1>
+        <h1 className="py-3 text-3xl font-bold text-base-text">
+          Choose an assistant for the perfect match
+        </h1>
 
+        <AlgoSelector />
+      </div>
 
       <div className="flex flex-col justify-around m-6 h-80 bg-primary-color rounded-3xl">
-        <h1 className="pt-3 text-3xl font-bold text-base-text">Modify your password</h1>
+        <h1 className="pt-3 text-3xl font-bold text-base-text">
+          Modify your password
+        </h1>
         <ModifyPasswordForm />
       </div>
-      
+
       <div className="flex flex-col justify-around m-6 h-52 bg-primary-color rounded-3xl">
-        <h1 className="pt-3 text-3xl font-bold text-base-text">Delete account !</h1>
+        <h1 className="pt-3 text-3xl font-bold text-base-text">
+          Delete account !
+        </h1>
         <DeleteAccountForm />
       </div>
     </div>
