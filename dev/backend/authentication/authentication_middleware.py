86eb0e8625dbefef9245af12eb27f7b295b926de
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-12-18 15:28:58 -0500

    headers backend done

--- dev/backend/authentication/authentication_middleware.py
+++ dev/backend/authentication/authentication_middleware.py
@@ -1,27 +1,32 @@
+'''
+------------------------------------------------------------------------------------
+====================================================================================
+Filename    : authentication_middleware.py
+Created By  : Bettina-Sarah Janesch
+About       : Cette classe gère la validation et le renouvellement des sessions
+              d'utilisateur en vérifiant l'état des tokens JWT. Elle fournit une 
+              méthode pour vérifier si un token est valide ou expiré, et génère un 
+              nouveau token si nécessaire. Si le token est sur le point d'expirer ou 
+              déjà expiré, un nouveau token est créé et renvoyé, garantissant ainsi 
+              une gestion sécurisée de l'authentification utilisateur.
+====================================================================================
+------------------------------------------------------------------------------------
+'''
+
+
 from authentication.crypt_keeper import CryptKeeper
 from functools import wraps
-
-
-# def requires_token(my_function) -> function: 
-#     @wraps(my_function)
-#     def decorated(*args, **kwargs):
-#         print('i am wrapped')
-#             # retour des plusieurs param dans fonction
-#         return my_function(*args,**kwargs)
-#     return decorated
-
 
 class AuthenticationMiddleware:
     def __init__(self) -> None:
         pass
     
-    # @requires_token
     def check_session_validity(self, token:str) -> bool:
         cryptkeeper = CryptKeeper()
         is_valid, user_id = cryptkeeper.decode(token)
         if isinstance(is_valid, bool):
             return is_valid, user_id
-        # we generate a new token if token is expired OR almost expiring!!
+        # we generate a new token if token is expired OR almost expiring
         new_token = self.generate_token(user_id)
         print('new token: ', new_token)
         return new_token, user_id
